a5e36e2751274e9d6ac0cf0ee63ddb692ed52e1e
saabismi <vilho.syvahuoko@gmail.com>
2021-09-05 15:42:55 +0300

    aloitin tekemään reittihakua

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ const searchBtn = document.getElementById("submitSearchByLineNumber"); // button
 const resultLineNum = document.getElementById("lineInfoNum"); // paragraph element for showing the line number
 const resultLineText = document.getElementById("lineInfoText"); // paragraph element for showing the line route as a text
 
-const url = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql"; // API address
+const urlGQL = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql"; // API address
 
 // Function which will be ran when the button "submitSearchByLineNumber" is clicked
 function searchByLineNumber() {
     let query = searchBox.value;
 
-    fetch(url,{
+    fetch(urlGQL,{
         method: "POST",
         headers: { "Content-Type": "application/graphql" },
         body: `{
@@ -33,4 +33,25 @@ function searchByLineNumber() {
         console.log("error:", error);
     });
 
+}
+
+const searchForStart = document.getElementById("searchForStart");
+const searchForEnd = document.getElementById("searchForEnd");
+
+searchForStart.addEventListener("input", searchStartPlace);
+
+const lastStartPlaceSearch = 0;
+
+function searchStartPlace(e) {
+    let date = new Date(); // this is for checking when the latest query was made so there's not multiple queries in one second
+    let userQuery = e.target.value;
+    const urlAddr = "https://api.digitransit.fi/geocoding/v1/search?text=";
+    let query = `${urlAddr}${userQuery}&size=3`;
+
+    console.log(searchForStart.value + " at " + date.getTime());
+    console.log(query);
+
+    
+
+    //fetch(query)
 }
